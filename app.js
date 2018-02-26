a6c4bc843fe442fc6810b5ecec11115c122b0bc6
Hani Zahra <hanizahra@ool-ad0201ad.dyn.optonline.net>
2018-02-26 12:20:45 -0500

    got the inital scrape to work

--- app.js
+++ app.js
@@ -11,9 +11,19 @@ app.get('/scrape', function(req, res){
     let houseNo = encodeURIComponent('84');
     let street = encodeURIComponent('Withers Street');
     let boro = 3;
-    url = 'http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro='+boro + "&houseno=" + houseNo + "&street=" + street;
-    console.log('url: ', url);
-    request(url, function(error, response, html){
+    // url = 'http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro='+boro + "&houseno=" + houseNo + "&street=" + street;
+    // url = 'http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=3&houseno=84&street=Withers%20Street';
+
+    let options = {
+        url : 'http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=3&houseno=84&street=Withers%20Street',
+        headers:  {
+
+                'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'
+            }
+    }
+   
+    // console.log('url: ', url);
+    request(options, function(error, response, html){
         if(!error){
             var $ = cheerio.load(html);
             var json = { address : "", violations : "", complaints : "", propertyId: ""};
