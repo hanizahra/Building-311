d119233dcb8d134f795c6a806796f84ab98a7354
Hani Zahra <hanizahra@ool-ad0201ad.dyn.optonline.net>
2018-02-27 11:19:43 -0500

    trying to render json file

--- app.js
+++ app.js
@@ -5,30 +5,31 @@ const cheerio = require('cheerio');
 const app     = express();
 const logger = require('morgan');
 const pgp = require('pg-promise');
+const PORT = process.env.PORT || 3000;
 const mainUrl = 'http://a810-bisweb.nyc.gov/bisweb/';
+const path = require('path');
+const buildingRouter = require('./routes/building-router')
 var json = { address : "", violations : "", numComplaints: 0, complaints : {}, propertyId: "", floodZone: ""};
+var jsonObject = require('./newoutput.json')
 
 
 
-function firstPage(resolve)
-{
+function firstPage(resolve){
        console.log('first');
         let houseNo = encodeURIComponent('84'); //$('#inputnumber').val()
         let street = encodeURIComponent('Withers Street'); //$('#inputstreet').val()
         let boro = 3;
 
-        let options = 
-        {
+        let options = {
             url : 'http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro='+boro + "&houseno=" + houseNo + "&street=" + street,
             headers:  
             {
                 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'
             }
         }        
-        request(options, function(error, response, html)
-        {
-            if(!error)
-            {
+        
+        request(options, function(error, response, html){
+            if(!error){
                 console.log('first request ok');
                 let $ = cheerio.load(html);
                
@@ -74,29 +75,22 @@ function firstPage(resolve)
         });
 }
 
-function secondPage()
-{
+function secondPage(){
     // let binNum = encodeURIComponent('3068248');
-        let options = 
-        {
+        let options = {
             url : 'http://a810-bisweb.nyc.gov/bisweb/ComplaintsByAddressServlet?requestid=1&allbin=' + json.propertyId ,
-            headers:  
-            {
+            headers: {
                 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'
             }
         }
-        return new Promise(function(resolve2, reject2)
-        {
-            request(options, function(error, response, html)
-            {
+        return new Promise(function(resolve2, reject2) {
+            request(options, function(error, response, html) {
                 console.log('html: ', html);   
-                if(!error)
-                {
+                if(!error) {
                     console.log('json: ', json);
                     console.log('second request ok');
                     let $ = cheerio.load(html);
-                    $($('a[href^="OverviewForComplaintServlet"]')).each(function(index, value)
-                    {
+                    $($('a[href^="OverviewForComplaintServlet"]')).each(function(index, value){
                         let data = $(this);                
                         let complaintId = data.text();
                         console.log('complaint', complaintId);
@@ -105,32 +99,26 @@ function secondPage()
                     console.log('json: ', json);
                     resolve2(); 
                 }
-                else
-                {
+                else {
                     console.log('second request error');
                 }
             });
         });
 }
 
-function writeToFile()
-{
-     fs.writeFile('newoutput.json', JSON.stringify(json, null, 4), function(err)
-        {
-            console.log('File written. Check newoutput.json file in project directory.');
-        });  
+function writeToFile(){
+    fs.writeFile('newoutput.json', JSON.stringify(json, null, 4), function(err) {
+        console.log('File written. Check newoutput.json file in project directory.');
+    });  
 }
 
-app.get('/scrape', function(req, res, next)
-{
-    let promise = new Promise(function(resolve, reject)
-    {
+app.get('/scrape', function(req, res, next) {
+    res.render('index')
+    let promise = new Promise(function(resolve, reject) {
        firstPage(resolve);
-    }).then(function(stuff)
-    {
+    }).then(function(stuff) {
         return secondPage();
-    }).then(function(stuff2)
-    {
+    }).then(function(stuff2) {
         writeToFile();
         res.send('Check console.');
     })
@@ -139,36 +127,22 @@ app.get('/scrape', function(req, res, next)
 
 
 
-// app.get('/scrape', function(req, res, next){    // complaints descriptions
-
-//     console.log('second');
-//     // let houseNo = encodeURIComponent('84'); //$('#inputnumber').val()
-//     // let street = encodeURIComponent('Withers Street'); //$('#inputstreet').val()
-//     // let boro = 3;
-//     //let binNum = encodeURIComponent('3068248'); //this is hard coded and needs a way to get accessed
-
-//     // url = 'http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro='+boro + "&houseno=" + houseNo + "&street=" + street;
-//     // url = 'http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=3&houseno=84&street=Withers%20Street';
 
 
-   
-//     // console.log('url: ', url);
-//    
-//     next();
-// });
+// view config
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// app.get('/scrape', function(req, res, next)
-// {
-//     console.log('third');
-//     console.log(json);
-  
-// });
+// port config
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}, in ${app.get('env')} node.`);
+});
 
+// error handler
+app.get('*', (req, res) => {
+    res.status(404).send('404 not found.');
+});
 
-
-app.listen('3000')
-
-console.log('Port running on 3000');
 
 
 exports = module.exports = app;
---
+++ routes/building-router.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const buildingRouter = express.Router();
+
+
+
+module.exports = buildingRouter;
